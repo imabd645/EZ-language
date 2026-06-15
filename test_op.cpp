d9a02610c608650673c626d1dc7a746f2d7c7a89
Abdullah Masood <imabd645@gmail.com>
2026-06-15 19:24:14 +0500

    Removed the Unnecssary files

--- test_op.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "src/Bytecode.h"
-int main() {
-    std::cout << "CALL: " << (int)OpCode::CALL << std::endl;
-    std::cout << "OP_AWAIT: " << (int)OpCode::OP_AWAIT << std::endl;
-    std::cout << "POP: " << (int)OpCode::POP << std::endl;
-    std::cout << "PRINT: " << (int)OpCode::PRINT << std::endl;
-    return 0;
-}
